4af41dacaf1bf9d2aab8fb4fe3a26931cf1c389f
anirbanbackend <anisadhu@asthaitsolutions.com>
2025-03-04 17:12:24 +0530

    push

--- db.js
+++ db.js
@@ -1,17 +1,32 @@
 import mongoose from 'mongoose';
 
-const mongoURI = "mongodb+srv://anisadhu:klShQrQmRflyxn7Q@cluster0.edr5b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";  // Replace with your MongoDB URI
+const MONGODB_URI = "mongodb+srv://anisadhu:klShQrQmRflyxn7Q@cluster0.edr5b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";  // Replace with your MongoDB URI
+;
+
+if (!MONGODB_URI) {
+    throw new Error("⚠️ MongoDB URI is missing in environment variables.");
+}
+
+let cached = global.mongoose || { conn: null, promise: null };
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-    process.exit(1); // Exit the process with failure code
-  }
+    if (cached.conn) return cached.conn;
+
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }).then((mongoose) => {
+            console.log("✅ MongoDB Connected");
+            return mongoose;
+        }).catch((err) => {
+            console.error("❌ MongoDB Connection Error:", err.message);
+            process.exit(1);
+        });
+    }
+
+    cached.conn = await cached.promise;
+    return cached.conn;
 };
 
 export default connectDB;
